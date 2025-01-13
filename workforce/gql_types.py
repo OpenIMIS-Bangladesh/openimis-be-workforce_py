e62946be09a1d37d0ab3e43926bf2afd5a3b945d
theeMahmud <mahmudrajucse@gmail.com>
2025-01-13 16:56:16 +0600

    gql_types

--- workforce/gql_types.py
+++ workforce/gql_types.py
@@ -24,7 +24,7 @@ class WorkforceOrganizationInputType(OpenIMISMutation.Input):
     id = graphene.String(required=False)
     name_bn = graphene.String()
     name_en = graphene.String(required=True)
-    location = graphene.String(required=True)
+    location_id = graphene.String(required=True)
     address = graphene.String()
     phone_number = graphene.String()
     email = graphene.String()
