5e5165efc08147d9b31ab329359130bb2bb002a2
theeMahmud <mahmudrajucse@gmail.com>
2025-02-10 16:09:21 +0600

    user add with related permissions

--- workforce/services/user_services.py
+++ workforce/services/user_services.py
@@ -1,5 +1,5 @@
-from core.models import InteractiveUser
-
+from core.models import InteractiveUser, User
+from location.models import UserDistrict
 
 def create_interactive_user(last_name, other_names, login_name, role_id):
     user = InteractiveUser.objects.create(
@@ -12,6 +12,21 @@ def create_interactive_user(last_name, other_names, login_name, role_id):
             "role_id": role_id,
             "private_key": "C1C224B03CD9BC7B6A86D77F5DACE40191766C485CD55DC48CAF9AC873335D6F",
             "password": "59E66831C680C19E8736751D5480A7C3291BD8775DF47C19C4D0361FBC1C3438"
+        }
+    )
+
+    user_add_core_user = User.objects.create(
+        **{
+            "username": user.login_name,
+            "i_user_id": user.id,
+        }
+    )
+
+    user_district_class = UserDistrict.objects.create(
+        **{
+            "user_id": user.id,
+            "location_id": 20,
+            "audit_user_id": -1,
         }
     )
 
