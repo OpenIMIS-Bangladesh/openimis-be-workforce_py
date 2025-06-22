19973f2b36c03f14d01789f98c212ffdeb585906
Nafi Shahriyar <imnafi404@gmail.com>
2025-06-22 16:42:25 +0600

    applicarion data format

--- workforce/services/workforce_application_summary_services.py
+++ workforce/services/workforce_application_summary_services.py
@@ -1,4 +1,6 @@
 import logging
+import json
+from django.core.exceptions import ValidationError
 
 from core.services import BaseService
 from workforce.models import WorkforceApplicationSummary
@@ -10,6 +12,16 @@ class WorkforceApplicationSummaryServices(BaseService):
     OBJECT_TYPE = WorkforceApplicationSummary
 
     def create(self, obj_data):
+        application_data = obj_data.get("application_data") or {}
+
+        if isinstance(application_data, str):
+            try:
+                application_data = json.loads(application_data)
+            except json.JSONDecodeError as e:
+                raise ValidationError(f"Invalid JSON in application_data: {str(e)}")
+
+        obj_data["application_data"] = application_data
+
         return super().create(obj_data)
 
     def update(self, obj_data):
