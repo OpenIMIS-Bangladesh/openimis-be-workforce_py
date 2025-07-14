19a89e1cff828f47916f3b1713961bd492fb9996
theeMahmud <mahmudrajucse@gmail.com>
2025-07-14 19:16:38 +0600

    logo

--- workforce/urls.py
+++ workforce/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FileUploadView, FileRetrieveView
+from .views import FileUploadView, FileRetrieveView, LogoRetrieveView
 
 urlpatterns = [
     path('document/upload', FileUploadView.as_view(), name='document-upload'),
     path('document/view/<str:filename>', FileRetrieveView.as_view(), name='document-view'),
+    path('logo', LogoRetrieveView.as_view(), name='logo-view'),
 ]
--- workforce/views.py
+++ workforce/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, FileResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAuthenticated
-from workforce.services.file_services import save_uploaded_file, retrieve_file_response
+from .services.file_services import save_uploaded_file, retrieve_file_response
+import os
 
 
 class FileUploadView(APIView):
@@ -29,3 +29,16 @@ class FileRetrieveView(APIView):
             return retrieve_file_response(filename)
         except Http404 as e:
             raise e
+
+
+class LogoRetrieveView(APIView):
+    def get(self, request):
+        try:
+            file_path = os.path.join(os.path.dirname(__file__), 'openIMIS.png')
+            logo = open(file_path, 'rb')
+            response = FileResponse(logo, content_type="image/png")
+            response['Content-Disposition'] = f'inline; filename="Logo"'
+
+            return response
+        except FileNotFoundError as e:
+            raise Http404(f"Error retrieving logo: {str(e)}")
