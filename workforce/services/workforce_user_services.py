fa3b10c8ca274bd1662616ea58421fd976ae832c
Nafi Shahriyar <imnafi404@gmail.com>
2025-07-30 19:49:31 +0600

    fixed user signup

--- workforce/services/workforce_user_services.py
+++ workforce/services/workforce_user_services.py
@@ -5,9 +5,16 @@ from django.db.models import Q
 
 from workforce.models import WorkforceUser
 from workforce.services.workforce_employee_services import WorkforceEmployeeServices
-from core.models import User
+from core.models import User, InteractiveUser
+from django.core.exceptions import ValidationError
 
 logger = logging.getLogger(__name__)
+
+
+def clean_value(value):
+    if isinstance(value, str):
+        value = value.strip()
+    return value or None
 
 
 class WorkforceUserServices():
@@ -15,6 +22,10 @@ class WorkforceUserServices():
 
     def create(self, obj_data):
         login_name = obj_data.get('nid') or obj_data.get('birth_certificate_no')
+
+        if InteractiveUser.objects.filter(validity_to__isnull=True, login_name=login_name).exists():
+            raise ValidationError(f"User with login name '{login_name}' already exists.")
+
         data = {
             "username": login_name,
             "other_names": ' ',
@@ -31,25 +42,26 @@ class WorkforceUserServices():
         create_user_id = created_user[0].id
 
         user_obj = WorkforceUser.objects.create(
-            name_bn=obj_data.get("name_bn"),
-            first_name_en=obj_data.get("first_name_en"),
-            last_name_en=obj_data.get("last_name_en"),
-            nid=obj_data.get("nid"),
-            phone_number=obj_data.get("phone_number"),
-            status=obj_data.get("status"),
+            name_bn=obj_data.get("name_bn", None),
+            first_name_en=obj_data.get("first_name_en", None),
+            last_name_en=obj_data.get("last_name_en", None),
+            nid=clean_value(obj_data.get("nid", None)),
+            birth_certificate_no=clean_value(obj_data.get("birth_certificate_no")),
+            phone_number=clean_value(obj_data.get("phone_number", None)),
+            status=obj_data.get("status", None),
         )
 
         user = User.objects.get(i_user=1)
 
         employee_service = WorkforceEmployeeServices(user)
         employee_data = {
-            "first_name_bn": obj_data.get("name_bn"),
-            "last_name_bn": "",
-            "first_name_en": obj_data.get("first_name_en"),
-            "last_name_en": "",
-            "nid": obj_data.get("nid"),
-            "phone_number": obj_data.get("phone_number"),
-            "birth_certificate_no": obj_data.get('birth_certificate_no'),
+            "first_name_bn": obj_data.get("name_bn", None),
+            "last_name_bn": None,
+            "first_name_en": obj_data.get("first_name_en", None),
+            "last_name_en": None,
+            "nid": clean_value(obj_data.get("nid", None)),
+            "phone_number": clean_value(obj_data.get("phone_number", None)),
+            "birth_certificate_no": clean_value(obj_data.get("birth_certificate_no")),
             "related_user_id": create_user_id,
         }
         employee_create = employee_service.create(employee_data)
@@ -58,10 +70,10 @@ class WorkforceUserServices():
 
     def update(self, obj_data):
         data = {
-            "username": obj_data.get('nid'),
+            "username": obj_data.get('nid', None),
             "other_names": ' ',
-            "last_name": obj_data.get('last_name_en'),
-            "phone": obj_data.get('phone_number'),
+            "last_name": obj_data.get('last_name_en', None),
+            "phone": obj_data.get('phone_number', None),
             "email": '',
             "language": 'en',
             "health_facility_id": 1,
