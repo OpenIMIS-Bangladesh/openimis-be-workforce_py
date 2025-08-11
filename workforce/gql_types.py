929a14c9412bcd66d97fb7917f7c7b333cc9df33
Nafi Shahriyar <imnafi404@gmail.com>
2025-08-11 10:42:43 +0600

    gql update

--- workforce/gql_types.py
+++ workforce/gql_types.py
@@ -146,7 +146,7 @@ class WorkforceFactoryInputType(OpenIMISMutation.Input):
     status = graphene.String()
     association_type = graphene.String()
     is_same_company_representative = graphene.String()
-    workforce_representative_id = graphene.UUID()
+    workforce_representative_id = graphene.String()
 
 
 class WorkforceEmployeeInputType(OpenIMISMutation.Input):
