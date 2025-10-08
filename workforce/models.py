cf74dd9027bba6cb134548a01c56ba5d6d312281
Nafi Shahriyar <imnafi404@gmail.com>
2025-10-08 17:42:47 +0600

    workforce_factory

--- workforce/models.py
+++ workforce/models.py
@@ -322,7 +322,13 @@ class WorkforceEmployee(HistoryModel):
     employee_id = models.CharField(
         max_length=255, null=True, blank=True, unique=True)
     employee_id_lima = models.CharField(max_length=255, null=True, blank=True)
-    workforce_factory_id = models.CharField(max_length=255, null=True, blank=True)
+    workforce_factory = models.ForeignKey(
+        WorkforceFactory,
+        models.DO_NOTHING,
+        blank=True,
+        null=True,
+        related_name="workforce_employee_factory",
+    )
     insurance_number = models.CharField(max_length=50, null=True, blank=True)
     employee_type = models.CharField(
         max_length=16, null=True, blank=True, db_comment="office/factory employee")
