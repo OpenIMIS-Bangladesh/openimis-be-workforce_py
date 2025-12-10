2ee1fe1b4c310b6609db900b840a5bbcda217e1d
Tahir082 <tahir.ewu@gmail.com>
2025-12-10 18:36:36 +0600

    raduan vai

--- workforce/schema.py
+++ workforce/schema.py
@@ -246,6 +246,7 @@ class Query(graphene.ObjectType):
     workforce_eis_payment_process = graphene.List(
         WorkforceEisPaymentProcessGQLType,
         workforce_application_id=graphene.String(),
+        workforce_application_id_in=graphene.List(of_type=graphene.String),
         month=graphene.String(),
         year=graphene.String(),
     )
@@ -1207,11 +1208,13 @@ class Query(graphene.ObjectType):
             return None
 
 
-    def resolve_workforce_eis_payment_disbursement(self, info, workforce_application_id=None, month=None, year=None):
+    def resolve_workforce_eis_payment_disbursement(self, info, workforce_application_id=None, workforce_application_id_in=None, month=None, year=None):
         try:
             qs = WorkforceEisPaymentDisbursement.objects.all()
             if workforce_application_id:
                 qs = qs.filter(workforce_application_id=workforce_application_id)
+            if workforce_application_id_in:
+                qs = qs.filter(workforce_application_id__in=workforce_application_id_in)
             if month:
                 qs = qs.filter(month_index=month)
             if year:
