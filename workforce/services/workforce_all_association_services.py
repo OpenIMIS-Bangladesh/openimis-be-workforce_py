a16e885bef38fa1f1d25f5fbd182bbc3ff397d4a
Nafi Shahriyar <imnafi404@gmail.com>
2026-01-01 15:05:47 +0600

    association update user id

--- workforce/services/workforce_all_association_services.py
+++ workforce/services/workforce_all_association_services.py
@@ -10,8 +10,10 @@ class WorkforceAllAssociationServices(BaseService):
     OBJECT_TYPE = WorkforceAllAssociation
 
     def create(self, obj_data):
+        obj_data["user_created"] = self.user
         return super().create(obj_data)
 
     def update(self, obj_data):
+        obj_data["user_updated"] = self.user
         return super().update(obj_data)
 
