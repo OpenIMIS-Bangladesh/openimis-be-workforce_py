8ea2875ba9b01fb01d16de8858d249d276dac6f9
Tahir082 <tahir.ewu@gmail.com>
2026-03-12 15:34:08 +0600

    application bank document fixed

--- workforce/models.py
+++ workforce/models.py
@@ -1496,6 +1496,9 @@ class WorkforceEisPaymentProcess(HistoryModel):
     trimonthly_amount= models.DecimalField(max_digits=25, decimal_places=5, null=True, blank=True)
     payable_amount= models.DecimalField(max_digits=25, decimal_places=5, null=True, blank=True)
     phone_number= models.CharField(max_length=50, null=True, blank=True)
+    arrear_amount= models.DecimalField(max_digits=25, decimal_places=5, null=True, blank=True)
+    arrear_payment_month= models.IntegerField(null=True, blank=True)
+    arrear_payment_year= models.IntegerField(null=True, blank=True)
 
     class Meta:
         managed = True
