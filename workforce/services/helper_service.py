42708f0591081bd84099d78a2680da9a8a1577af
Tahir082 <tahir.ewu@gmail.com>
2026-07-02 14:48:08 +0600

    done

--- workforce/services/helper_service.py
+++ workforce/services/helper_service.py
@@ -172,14 +172,21 @@ def generate_beneficiary_id(association, application_id=None, dependent_count=No
     application = WorkforceApplication.objects.filter(id=application_id).first()
     if application is None:
         return ""
-    all_applications = WorkforceApplication.objects.filter(organization_type="eis")
+    # all_applications = WorkforceApplication.objects.filter(organization_type="eis")
     # all_applications = WorkforceApplication.objects.filter(organization_type="eis", status="verified").order_by("-date_created")
-    position_in_table=1
-    for app in all_applications:
-        if app.id== application.id:
-            break
-        else:
-            position_in_table=position_in_table+1
+
+    # for app in all_applications:
+    #     if app.id== application.id:
+    #         break
+    #     else:
+    #         position_in_table=position_in_table+1
+    count_by_distinct_application = (
+        WorkforceEisPaymentProcess.objects
+        .values("workforce_application")
+        .distinct()
+        .count()
+    )
+    position_in_table=count_by_distinct_application+1
     sequential_serial_number= str(position_in_table).zfill(6)
     accident_info= json.loads(application.employee_accident_info) if application.employee_accident_info!="{}" else None
     accident_type="1"
