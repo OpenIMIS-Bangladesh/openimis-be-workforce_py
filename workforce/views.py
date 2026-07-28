1a637dae8d41b2ef417b8808e7b569a16f4b45b0
Tahir082 <tahir.ewu@gmail.com>
2026-07-28 14:57:25 +0600

    file deleting procedure test

--- workforce/views.py
+++ workforce/views.py
@@ -687,21 +687,15 @@ class DeleteOrphanFiles(APIView):
                 # e.g. /file_storage/content/workforce/abc.pdf
                 storage_url = default_storage.url(relative_path)
 
-                matched= 0
-                countindb=0
-                for path in db_paths:
-                    countindb+=1
                 if storage_url not in db_paths:
                     if dry_run:
                         print(f"Would delete: {storage_url}")
                         deleted += 1
-                        for path in db_paths:
-                            if storage_url == path:
-                                matched+=1
                     else:
                         default_storage.delete(relative_path)
                         print(f"Deleted: {storage_url}")
                         deleted += 1
+
         print(f"Deleted {deleted} orphan files.")
-        return Response({'status': 'success', 'message': 'Data Retrieved Successfully', 'data': f"Deleted {deleted} orphan files. but count in db: {countindb}. Match: {matched}"},
+        return Response({'status': 'success', 'message': 'Data Retrieved Successfully', 'data': f"Deleted {storage_url} orphan files."},
                         status=status.HTTP_200_OK)
