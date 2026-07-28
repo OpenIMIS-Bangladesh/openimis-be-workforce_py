307297679fd116174c8c67c6b155243237015663
Tahir082 <tahir.ewu@gmail.com>
2026-07-28 15:50:22 +0600

    try monthly installment fixed

--- workforce/services/workforce_eis_payment_disbursement_stage_services.py
+++ workforce/services/workforce_eis_payment_disbursement_stage_services.py
@@ -147,8 +147,8 @@ class WorkforceEisPaymentDisbursementStageServices(BaseService):
 
             # FIRST DISBURSEMENT ARREAR CALCULATION NUMBER OF MONTHS END ================
             prev_stage_data= WorkforceEisPaymentDisbursementStage.objects.filter(workforce_eis_payment_process_id= process_id, is_deleted=False, month_index__lt=int(month), year=year).first()
-            if prev_stage_data is not None:
-                months_gone=1 #it will be multiplied with payable amount so reset it to 1 if this is not first payment.
+            if prev_stage_data is not None or workforce_eis_payment_process.eis_payment_type!="monthly":
+                months_gone=1 #it will be multiplied with payable amount so reset it to 1 if this is not first payment or not monthly payment.
 
             if safe_decimal(workforce_eis_payment_process.arrear_payment_month) == safe_decimal(month) -1 and safe_decimal(workforce_eis_payment_process.arrear_payment_year) == safe_decimal(year):
                 paid_amount = safe_decimal(workforce_eis_payment_process.payable_amount) + safe_decimal(workforce_eis_payment_process.arrear_amount)
